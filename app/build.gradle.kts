c29ba4f83659e076e1705187174e042c437004e4
symph0nic <80276348+symph0nic@users.noreply.github.com>
2025-10-24 17:31:03 +0200

    Buildversion fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.jonmechan.quicknote"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jonmechan.quicknote"
